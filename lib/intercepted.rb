9a9dd5199cb9a40bb40c48d7e52060482e6d81f3
Yegor Bugayenko <yegor256@gmail.com>
2025-06-24 14:56:06 +0300

    #3: Refactor method_missing logic and add named argument support.

--- lib/intercepted.rb
+++ lib/intercepted.rb
@@ -5,16 +5,31 @@
 
 # System module.
 module Kernel
-  def intercepted(origin = nil, &pipe)
+  def intercepted(origin = nil)
     raise 'Block is expected by intercepted()' unless block_given?
 
     Class.new(BasicObject) do
       define_method(:initialize) { @o = origin }
-      define_method(:method_missing) do |name, *args, &block|
-        pipe.call(@o, name, *args, &block)
+      define_method(:method_missing) do |mtd, *args, &block|
+        yield(:before, mtd, args)
+        params = @o.method(mtd).parameters
+        reqs = params.count { |p| p[0] == :req }
+        maybe = %i[key keyreq]
+        r =
+          if params.any? { |p| maybe.include?(p[0]) } && args.size > reqs
+            @o.__send__(mtd, *args[0..-2], **args.last) do |*a|
+              block&.call(*a)
+            end
+          else
+            @o.__send__(mtd, *args) do |*a|
+              block&.call(*a)
+            end
+          end
+        yield(:after, mtd, args, r)
+        r
       end
-      define_method(:respond_to?) do |name, include_private = false|
-        @o.respond_to?(name, include_private)
+      define_method(:respond_to?) do |mtd, include_private = false|
+        @o.respond_to?(mtd, include_private)
       end
     end.new
   end
--- test/test_intercepted.rb
+++ test/test_intercepted.rb
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: MIT
 
 require_relative '../lib/intercepted'
+require_relative 'test__helper'
 
 # Test.
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
@@ -11,23 +12,46 @@ require_relative '../lib/intercepted'
 # License:: MIT
 class Testintercepted < Minitest::Test
   def test_simple
-    i = intercepted(42) do |o, name, *args, &block|
-      r = o.__send__(name, *args, &block)
-      puts "#{name}(#{args.join(', ')}) -> #{r}"
-      r
+    i = intercepted(42) do |e, m, args, r|
+      puts "#{m}(#{args.join(', ')}) -> #{r}" if e == :after
     end
     assert_equal(46, i + 4)
     assert_respond_to(i, :+)
   end
 
-  def test_with_named_parameters
-    a = nil
-    i = intercepted(42) do |_o, name, *args|
-      a = { name:, args: }
+  def test_only_passes_named_arguments
+    o = Class.new do
+      def foo(bar: 0)
+        bar + yield
+      end
+    end.new
+    i = intercepted(o) do |_e, _m, _args, _r|
+      'none'
     end
-    i.foo('hey', x: 42)
-    assert_equal(:foo, a[:name])
-    assert_equal('hey', a[:args][0])
-    assert_equal(42, a[:args][1][:x])
+    assert_equal(11, i.foo(bar: 8) { 3 })
+  end
+
+  def test_passes_named_arguments_through
+    o = Class.new do
+      def foo(one, bar: 0)
+        one + bar + yield
+      end
+    end.new
+    i = intercepted(o) do |_e, _m, _args, _r|
+      true
+    end
+    assert_equal(21, i.foo(7, bar: 8) { 6 })
+  end
+
+  def test_passes_stared_args
+    o = Class.new do
+      def foo(*_args)
+        yield
+      end
+    end.new
+    i = intercepted(o) do |_e, _m, _args, _r|
+      true
+    end
+    assert_equal(77, i.foo(8, 8, 8) { 77 })
   end
 end
